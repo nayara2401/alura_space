2467cc9b1d2b12d628f34b82f84a504dddb2ebc0
Nayara Pereira <nayarapereira2401@gmail.com>
2025-09-13 01:23:47 -0300

    Commit para arquivos do alura space

---
+++ galeria/models.py
@@ -0,0 +1,24 @@
+from django.db import models
+
+from datetime import datetime
+
+class Fotografia(models.Model):
+
+    OPCOES_CATEGORIA = [
+    ("NEBULOSA", "Nebulosa"),
+    ("GALAXIA", "Galáxia"),
+    ("PLANETA", "Planeta"),
+    ("ESTRELA", "Estrela"),
+     
+    ]
+
+    nome = models.CharField(max_length=100, null=False, blank=False)
+    legenda = models.CharField(max_length=150, null=False, blank=False)
+    categoria = models.CharField(max_length=50, choices=OPCOES_CATEGORIA, default='')
+    descricao = models.TextField(null=False, blank=False)
+    foto = models.ImageField(upload_to='fotos/%Y/%m/%d/', blank=True)    
+    publicada = models.BooleanField(default=False)
+    data_fotografia = models.DateTimeField(default=datetime.now, blank=True)
+
+    def __str__(self):
+        return self.nome
